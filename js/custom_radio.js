c65483951d0b98bc478f6a23955eb1f021a549cd
cisco <cisco>
2016-05-10 12:03:05 +0200

    On peut avoir plusieurs customs sur la meme page

---
+++ js/custom_radio.js
@@ -0,0 +1,25 @@
+
+$(function(){
+
+	//on ajoute un span après chaque input et on cache l'input	
+	$('.custom_radio input[type=radio]').hide().after('<span class="off"></span>');
+
+	//evt de click sur les spans
+	$('.off').click(function(){
+		//on deselectionne le precedent
+		$('.on').prev().prop('checked', false);
+		$('.on').removeClass('on').addClass('off');
+
+		//on selectionne l'elt en cours
+		$(this).removeClass('off').addClass('on');
+		$(this).prev().prop('checked', true);
+
+	});
+
+
+	//verif saisie
+	$('#btn').click(function(){
+		console.log($('input[name=choix]:checked').val());
+		console.log($('input[name=sexe]:checked').val());
+	});
+});
